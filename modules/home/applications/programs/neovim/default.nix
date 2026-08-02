4296b3bc9228df7de6302f6c186c8b2e3f387068
alljokecake <ilhan@duck.com>
2026-08-01 17:43:36 -0700

    refactor(neovim): use external neovim configuration
    - remove the bundled neovim configuration from nix-config
    - configure hm to use an out-of-store symlink
    - manage the neovim configuration in its own repository

--- modules/home/applications/programs/neovim/default.nix
+++ modules/home/applications/programs/neovim/default.nix
@@ -1,17 +1,16 @@
 {
-  flake.modules.homeManager.neovim =
-  {
+  flake.modules.homeManager.neovim = { config, lib, ... }: {
     programs.neovim = {
       enable = true;
       defaultEditor = true;
       vimAlias = true;
     };
 
-    xdg.configFile = {
-      "nvim" = {
-        source = ./nvim;
-        recursive = true;
-      };
-    };
+    # Trying to configure neovim through hm while keeping the lua style config made
+    # a ton of mess, I don't think it's a viable option
+    xdg.configFile."nvim/init.lua".enable = lib.mkForce false;
+    xdg.configFile."nvim".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nvim";
+
   };
 }
